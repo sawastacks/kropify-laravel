206a04bf8969e6a68fce5509560d903f18ad125b
MB'DUSENGE Callixte <77852570+sawastacks@users.noreply.github.com>
2024-10-26 10:08:56 +0300

    ...

---
+++ src/KropifyServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright (c) 2023 - present
+ * Kropify - KropifyServiceProvider.php
+ * Author: Sawa Stacks - sawastacks@gmail.com
+ * GitHub : github.com/sawastacks
+ * Initial version created on: 10/03/2023
+ * MIT license: https://github.com/sawastacks/kropify-laravel/blob/master/LICENSE
+ */
+
+namespace SawaStacks\Utils;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+/**
+ * Class GropyServiceProvider
+ */
+class KropifyServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string
+     */
+    const PACKAGE_PUBLIC_ASSETS_PATH = 'vendors/sawastacks/gropy';
+    /**
+     * Register services.
+     * @return void
+     */
+    public function register(): void
+    {
+         $this->app->bind('kropify', function(){
+            return new Kropify();
+         });
+    }
+
+    /**
+     * Bootstrap services.
+     * @return void
+     */
+    public function boot(): void
+    {
+        $package_public_assets_path = self::PACKAGE_PUBLIC_ASSETS_PATH;
+
+        if ( $this->app->runningInConsole() ) {
+            $viewPath = __DIR__ . '/'.'resources/assets';
+            $this->publishes([
+                $viewPath => public_path($package_public_assets_path),
+            ], 'kropify-assets');
+        }
+
+        Blade::directive('kropifyStyles', function () use ($package_public_assets_path) {
+            return '<link href="/' . $package_public_assets_path . '/css/kropify.min.css" rel="stylesheet">';
+        });
+
+        Blade::directive('kropifyScripts', function () use ($package_public_assets_path) {
+            return '<script src="/' . $package_public_assets_path . '/js/kropify.min.js"></script>';
+        });
+        
+    }
+}
